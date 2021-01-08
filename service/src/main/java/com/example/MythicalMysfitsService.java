5ee9219f88bb232d39a343be8118fa242349860d
unknown <you@example.com>
2021-01-08 21:10:06 +1100

    Initial commit

---
+++ service/src/main/java/com/example/MythicalMysfitsService.java
@@ -0,0 +1,49 @@
+package com.example;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class MythicalMysfitsService {
+
+
+    public Mysfits getAllMysfits() {
+
+        List<Mysfit> mysfits = new ArrayList<>();
+        mysfits.add(getMysfit("1L"));
+        Mysfits allMysfits = new Mysfits(mysfits);
+
+        return allMysfits;
+    }
+
+
+    public Mysfit getMysfit(String mysfitId){
+        
+        Mysfit mysfit = new Mysfit();
+        mysfit.setMysfitId(mysfitId);
+        mysfit.setName("Rygard");
+        return mysfit;
+    }
+
+    public void likeMysfit(String mysfitId) {
+
+        Mysfit mysfitToUpdate = new Mysfit();
+        mysfitToUpdate.setMysfitId(mysfitId);
+        mysfitToUpdate.setName("Rygard");               
+        Integer likes = mysfitToUpdate.getLikes() + 1;
+        mysfitToUpdate.setLikes(likes);    
+    }
+
+    public void adoptMysfit(String mysfitId) {
+        Mysfit mysfitToUpdate = new Mysfit();
+        mysfitToUpdate.setAdopted(true);
+    }
+
+    public Mysfits queryMysfits(String filter, String value) {
+        Mysfits allMysfits = getAllMysfits();
+        return allMysfits;
+    }
+
+}
