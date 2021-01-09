28f4c26818e83c9b10c3d13f8f6aff4e0de241a7
mxcheung <mxcheung@users.noreply.github.com>
2021-01-09 18:17:37 +1100

    Update MythicalMysfitsService.java

--- service/src/main/java/com/example/MythicalMysfitsService.java
+++ service/src/main/java/com/example/MythicalMysfitsService.java
@@ -23,7 +23,7 @@ public class MythicalMysfitsService {
         
         Mysfit mysfit = new Mysfit();
         mysfit.setMysfitId(mysfitId);
-        mysfit.setName("Rygard 1.4");
+        mysfit.setName("Rygard 1.51");
         return mysfit;
     }
 
